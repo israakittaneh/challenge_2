776b8bed981af64bf1db23c5ec1f3022418f6042
israakittaneh <israakittanehm@gmail.com>
2020-11-03 15:57:01 +0200

    never gonna let you down

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
+var express = require('express');
+const bodyParser = require('body-parser');
 
 
+var app = express();
+var port = 4000;
+app.use(express.static('client'));
+
+app.listen(port, (err, result) => {
+    if (err) {
+        console.log('This is the error', err);
+    }
+    console.log('CSV Generator Starting Up...');
+});
+
+app.use(express.static(__dirname + '/client'));
+app.use(bodyParser.text());
